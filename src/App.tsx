4645adb9ad3e1b1df52833c5d92363946daf659c
Hector <torrezhectorb@gmail.com>
2024-03-06 15:45:22 -0600

    refactor: add a validation to show an error when the api get nothing

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,41 @@ import CurrentDayCard from "./components/CurrentDay/CurrentDayCard";
 import Navbar from "./components/Navbar/Navbar";
 import useGetWeather from "./hooks/useGetWeather";
 import WeatherForecast from "./components/WeatherForecast/WeatherForecast";
+import { useState } from "react";
 
 function App() {
-  const { weather } = useGetWeather();
+  const [input, setInput] = useState("san salvador");
+
+  const handleSearch = (search: string) => {
+    setInput(search);
+  };
+  const { weather } = useGetWeather(input);
 
   return (
     <main className={styles["main"]}>
-      <Navbar />
+      <Navbar handleSearch={handleSearch} />
       <section className={styles["current-day"]}>
         <p className={styles["current-day--title"]}>Current Day</p>
-        {weather.map((day) => (
-          <>
-            <CurrentDayCard
-              key={day.city.id}
-              city={day.city}
-              currentDay={day.list[0]}
-            />
-            <section className={styles["forecast-days"]}>
-              {day.list.map((item) => {
-                return (
-                  <WeatherForecast
-                    key={crypto.randomUUID()}
-                    currentDay={item}
-                  />
-                );
-              })}
-            </section>
-          </>
-        ))}
+
+        <>
+          {weather?.map((day) =>
+            day.cod === "200" ? (
+              <section key={day.city.id}>
+                <CurrentDayCard
+                  city={day.city}
+                  currentDay={day?.list[0] ?? []}
+                />
+                <section className={styles["forecast-days"]}>
+                  {day?.list.map((item, index) => {
+                    return <WeatherForecast key={index} currentDay={item} />;
+                  })}
+                </section>
+              </section>
+            ) : (
+              <div>{day.message}</div>
+            )
+          )}
+        </>
       </section>
     </main>
   );
