f24528c92befc9a9f74c5b953cdf88af6fb907b5
Hector <torrezhectorb@gmail.com>
2024-03-07 09:12:47 -0600

    refactor: now the user can search by lat and lon

--- src/components/helpers/getWeather.ts
+++ src/components/helpers/getWeather.ts
@@ -1,12 +1,20 @@
 export const getWeather = async (city = "san salvador") => {
-  const API = `https://api.openweathermap.org/data/2.5/forecast?appid=${
-    import.meta.env.VITE_API_KEY
-  }&q=${city}&units=metric`;
+  let API;
 
-  // `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lot}&appid=${
-  //   import.meta.env.VITE_API_KEY
-  // }&units=metric`;
+  const coordinates = city.split("/");
 
+  if (coordinates.length === 2) {
+    const lat = coordinates[0].trim();
+    const lon = coordinates[1].trim();
+
+    API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&lang=sp&appid=${
+      import.meta.env.VITE_API_KEY
+    }&units=metric`;
+  } else {
+    API = `https://api.openweathermap.org/data/2.5/forecast?appid=${
+      import.meta.env.VITE_API_KEY
+    }&q=${city}&units=metric&lang=sp`;
+  }
   const response = await fetch(API);
   const data = await response.json();
 
